3608faaa7c2d9760df7b25ba21c4e953c191781b
Tsairenkun <Tsairenkun@gmail.com>
2020-08-03 15:49:54 +0800

    completed mini p2

--- miniproject2.rb
+++ miniproject2.rb
@@ -13,15 +13,28 @@ dial_book = {
  
 # Get city names from the hash
 def get_city_names(somehash)
+    somehash.keys
 # Write code here
 end
  
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
+    somehash[key]
 # Write code here
 end
  
 # Execution flow
 loop do
-# Write your program execution code here
+  puts "Do you want to lookup an area code based on city name? (Y/N)"
+  answer = gets.chomp.downcase
+  break if answer != 'y'
+  puts "which city do you want to lookup the areacode for?"
+  puts get_city_names(dial_book)
+  print "City name: "
+  city_name = gets.chomp.downcase
+  if dial_book.include?(city_name)
+    puts "the area code for #{city_name} is #{get_area_code(dial_book,city_name)}"
+  else
+    puts "City is not in dial book"
+  end
 end
